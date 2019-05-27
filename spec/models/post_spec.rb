a1fbc5c0189daf9de7160c0aab9ce6fbb7c74193
sebastian <sebazecenarro@gmail.com>
2019-05-26 23:32:44 -0300

    homework 1

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,4 +3,26 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   # En base al modelo de test propuesto para el modelo User,
   # implementar los tests para el modelo Post.
+  describe 'Factory' do
+    it 'has a valid factory' do
+      expect(build(:post)).to be_valid
+    end
+  end
+
+  describe 'Associations' do
+    it { should belong_to(:user) }
+    # https://github.com/thoughtbot/shoulda-matchers#activerecord-matchers
+  end
+
+  describe 'Presence validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+    # https://github.com/thoughtbot/shoulda-matchers#activemodel-matchers
+  end
+
+  describe 'Uniqueness validations' do
+    subject { create(:post) }
+    it { should validate_uniqueness_of(:title) }
+    # https://github.com/thoughtbot/shoulda-matchers#activemodel-matchers
+  end
 end
